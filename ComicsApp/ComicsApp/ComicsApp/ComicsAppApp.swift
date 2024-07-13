40650ba9cb4602cac4de2a53e80278c6450e39fd
yanelysvelarde <95549729+yanelysvelarde@users.noreply.github.com>
2024-07-13 02:20:37 -0400

    Add files via upload

---
+++ ComicsApp/ComicsApp/ComicsApp/ComicsAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  ComicsAppApp.swift
+//  ComicsApp
+//
+//
+
+import SwiftUI
+
+@main
+struct ComicsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComicsApp/ComicsApp/ComicsApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ComicsApp
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
